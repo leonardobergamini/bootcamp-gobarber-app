aedc257176c04620bd0806b0fd31dedb69b067aa
Leonardo Bergamini <leonardobergamini@MacBook-Air-de-Leonardo.local>
2020-02-20 22:20:53 -0300

    validação de senha no login

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -9,8 +9,8 @@ class SessionController {
 
         const schema = Yup.object().shape({
             email: Yup.string().email().required(),
-            password: Yup.string().required()
-        });
+            password: Yup.string().min(6).required()
+        })
 
         if(!(await schema.isValid(req.body))) {
             return res.status(400).json( { error: 'E-mail e senha obrigatórios.' } );
