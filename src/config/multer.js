e61fc235001e831e62bd3cac2f060f8f79952750
Leonardo Bergamini <leonardobergamini@MacBook-Air-de-Leonardo.local>
2020-02-20 22:21:50 -0300

    configuração do multer para upload de imagens

---
+++ src/config/multer.js
@@ -0,0 +1,19 @@
+const multer = require('multer');
+const crypto = require('crypto');
+const {extname, resolve} = require('path');
+
+module.exports = {
+    storage: multer.diskStorage({
+        destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+        filename: (req, file, callback) => {
+            crypto.randomBytes(16, (err, res) => {
+                if(err) return callback(err);
+
+                return callback(
+                    null, 
+                    res.toString('hex') + extname(file.originalname)
+                );
+            });
+        }
+    })
+}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 const { Router } = require('express');
+const multer = require('multer');
+
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 const authMiddleware = require('./app/middlewares/auth');
 
+const multerConfigs = require('./config/multer');
+
 const routes = new Router();
+const upload = multer(multerConfigs);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 routes.get('/users', UserController.index);
 routes.put('/users', authMiddleware, UserController.update);
+routes.post('/file', upload.single('file'), (req, res) => {
+    return res.json({ ok: true });
+});
 
 
 module.exports = routes;
