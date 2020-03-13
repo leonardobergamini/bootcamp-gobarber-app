8af4085b8b521bad21f5854b57df73d6314d3a4c
Leonardo Bergamini <leonardobergamini@MacBook-Air-de-Leonardo.local>
2020-03-12 22:06:36 -0300

    add  paginação para  a listagem dos agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,10 +7,15 @@ const File = require('../models/File');
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      // quantos registros deseja pular
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
